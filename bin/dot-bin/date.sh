bfe8351f076c09f642ce65a7fa6340fc098ffd50
Tracnac <tracnac@devmobs.fr>
2021-10-14 10:39:44 +0200

    OpenBSD conf change

--- bin/dot-bin/date.sh
+++ bin/dot-bin/date.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-notify-send -t 10000 "Information: " "`date +"%T%n %A %d %B %Y%n [%V|%Z|%z]";echo;cal `"
+notify-send -t 10000 "Information: " "`date +"%T%n %A %d %B %Y%n [%V|%Z|%z]";echo;cal -m`"
